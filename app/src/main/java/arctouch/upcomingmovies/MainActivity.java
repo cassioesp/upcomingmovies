641dfaed3c970c5dc9338c22071ea07651a6d33b
cassioesp <cagecc91>
2017-12-20 14:13:22 -0300

    first commit.

---
+++ app/src/main/java/arctouch/upcomingmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package arctouch.upcomingmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
